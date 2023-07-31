95e0dd941badc46464bde4172e824df5d98dfe25
Ivani_Chizhs <ivanchizhina@gmail.com>
2023-07-31 22:01:11 +0300

    Lesson1.2N

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ x = b ** 2 - (4 * a * c)
 
 if x < 0:
     print("Корней нет")
-else:
-    print((x ** 0.5) - b / (2 * a))
-    print(-b - (x ** 0.5) / (2 * a))
+elif x > 0:
+    print(((x ** 0.5) - b) / (2 * a))
+    print((-b - (x ** 0.5)) / (2 * a))
+elif x == 0:
+    print(((x ** 0.5) - b) / (2 * a))
