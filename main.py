93e4fb87c3c20d7aa47232038a9fd920e858c3e0
Ivani_Chizhs <ivanchizhina@gmail.com>
2023-08-14 02:37:10 +0300

    Lesson-3Dz.4

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-j = []
-b = input()
+a = ["qwert", "qw", "qwer", "qwe", "q"]
 
-while b != "стоп":
-    b = input()
-    j.append(b)
-
-print(j)
+print(a)
+for i in range(len(a)):
+    for j in range(len(a) - 1):
+        if a[j] > a[j + 1]:
+            a[j], a[j + 1] = a[j + 1], a[j]
+print(a)
