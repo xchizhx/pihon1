a37aa84142515529a01973e8761828aed82ab709
Ivani_Chizhs <ivanchizhina@gmail.com>
2023-08-14 18:50:04 +0300

    Lesson-3Dz.4.2

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ a = ["qwert", "qw", "qwer", "qwe", "q"]
 print(a)
 for i in range(len(a)):
     for j in range(len(a) - 1):
-        if a[j] > a[j + 1]:
+        if len(a[j]) > len(a[j + 1]):
             a[j], a[j + 1] = a[j + 1], a[j]
 print(a)
