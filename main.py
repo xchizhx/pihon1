24266a689832c1a27c7beddfff8b06537bf8f798
Ivani_Chizhs <ivanchizhina@gmail.com>
2023-08-25 14:13:42 +0300

    Lesson-5

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 from math import factorial
 
 
-rand_num = random.randit(-100, 100)
+rand_num = random.randint(-100, 100)
 rand_num_dict = {"key": rand_num}
 
-for i in range()
+print(rand_num_dict)
